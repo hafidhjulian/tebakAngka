7926909e454018278d787142178196f83047942e
Hafidh Kurniadi <hafidhjulian@192.168.1.8>
2019-02-03 13:43:44 +0700

    Initial Commit

---
+++ TebakAngka/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TebakAngka
+//
+//  Created by Hafidh Julian Kurniadi on 03/02/19.
+//  Copyright © 2019 Hafidh Julian Kurniadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
